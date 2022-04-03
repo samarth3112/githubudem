34c0613bca459404512f2b908b9c70b935c25b4e
samarth3112 <samarth.saxena@lns.maersk.com>
2022-04-04 02:13:45 +0530

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,24 +16,24 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-#Create Resource Group
-# resource "azurerm_resource_group" "tamops" {
-#   name     = "aerito"
-#   location = "eastus2"
-# }
+Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "aerito"
+  location = "eastus2"
+}
 
-# #Create Virtual Network
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "hr-vnet"
-#   address_space       = ["192.168.0.0/16"]
-#   location            = "eastus2"
-#   resource_group_name = azurerm_resource_group.tamops.name
-# }
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "hr-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.tamops.name
+}
 
-# # Create Subnet
-# resource "azurerm_subnet" "subnet" {
-#   name                 = "subnet"
-#   resource_group_name  = azurerm_resource_group.tamops.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefix       = "192.168.0.0/24"
-# }
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.tamops.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
+}
